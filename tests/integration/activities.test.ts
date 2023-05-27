4bafcd4a2a23f198fd92734a9109f616b08b322a
kendy-karakawa <kendy.karakawa@gmail.com>
2023-05-27 16:33:00 -0300

    feat: test get activities dates

---
+++ tests/integration/activities.test.ts
@@ -0,0 +1,52 @@
+import faker from '@faker-js/faker';
+import { TicketStatus } from '@prisma/client';
+import httpStatus from 'http-status';
+import * as jwt from 'jsonwebtoken';
+import supertest from 'supertest';
+import { createUser } from '../factories';
+import { cleanDb, generateValidToken } from '../helpers';
+import app, { init } from '@/app';
+
+beforeAll(async () => {
+  await init();
+});
+
+beforeEach(async () => {
+  await cleanDb();
+});
+
+const server = supertest(app);
+
+describe('GET /activities', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.get('/activities');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get('/activities').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get('/activities').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 200 and activities dates', async () => {
+    const user = await createUser();
+    const token = await generateValidToken(user);
+
+    const response = await server.get('/activities').set('Authorization', `Bearer ${token}`);
+    expect(response.status).toEqual(httpStatus.OK);
+    expect(Array.isArray(response.body)).toEqual(true);
+  });
+});
